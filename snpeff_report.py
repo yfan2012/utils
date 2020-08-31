9e9a0a9235a6fe665e7af33f1893297aa6ab2631
yfan2012 <yfan2012@gmail.com>
2020-08-31 18:40:56 -0400

    check on snpeff report since rscript counting is a bit weird

--- snpeff_report.py
+++ snpeff_report.py
@@ -6,35 +6,36 @@ take an annotation of the ref genome and find what genes are mutated
 
 def assign_snps(snpsfile):
     '''
-    input snp bedfile made from intersecting vcf and gff
+    input snp bedfile made from snpeff
     return list of lists [chr, pos, refbase, altbase, refobs, altobvs, feature, prodinfo, geneinfo, noteinfo]
     '''
     with open(snpsfile) as f:
-        content=f.read().split('\n')
+       content=f.read().split('\n')
     f.close()
     snpinfo=[]
     for i in content:
-        info=i.split('\t')
-        if len(i)==0:
-            continue
-        if i[0]=='#':
-            continue
-        ref=info[3]
-        gt=info[9].split(':')
-        alt=info[4].split(',')
-        ao=gt[5].split(',')
-        altinfo=list(zip(alt, ao))
-        ##sometimes snpeff leaves out bases
-        ro=gt[3]
-        snpeffann=info[7].split(';')[41].split(',')
-        snpeffann[0]=snpeffann[0][4:]
-        for var in snpeffann:
-            annot=var.split('|')
-            for alt in altinfo:
-                if annot[0] in alt[0]:
-                    annotnum=alt[1]
-                    snpinfo.append([info[0], info[1], ref, annot[0], ro, annotnum, annot[1], annot[2], annot[3], annot[5], annot[10], annot[15]])
+       info=i.split('\t')
+       if len(i)==0:
+           continue
+       if i[0]=='#':
+           continue
+       ref=info[3]
+       gt=info[9].split(':')
+       alt=info[4].split(',')
+       ao=gt[5].split(',')
+       altinfo=list(zip(alt, ao))
+       ro=gt[3]
+       snpeffann=info[7].split(';')[41].split(',')
+       snpeffann[0]=snpeffann[0][4:]
+       for var in snpeffann:
+           annot=var.split('|')
+           for alt in altinfo:
+               if annot[0] in alt[0]:
+                   annotnum=alt[1]
+                   snpinfo.append([info[0], info[1], ref, annot[0], ro, annotnum, annot[1], annot[2], annot[3], annot[5], annot[10], annot[15]])
     return(snpinfo)
+
+
 
 
 def main(snpsfile, outfile):
